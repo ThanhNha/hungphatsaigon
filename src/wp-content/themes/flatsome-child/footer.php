5986d3c6aad548192d84be8c98cf4de210f1371b
shin <nha.le@floatingcube.com>
2023-09-10 12:26:50 +0700

    done add modify

---
+++ src/wp-content/themes/flatsome-child/footer.php
@@ -0,0 +1,38 @@
+<?php
+
+/**
+ * The template for displaying the footer.
+ *
+ * @package          Flatsome\Templates
+ * @flatsome-version 3.16.0
+ */
+if (is_shop() || is_product_category()) : ?>
+    <?php echo do_shortcode('[recently_viewed_products][block id="danh-muc-noi-bat"]'); ?>
+<?php endif;
+
+
+global $flatsome_opt;
+?>
+<?php if ( !is_page(49107) ) : ?>
+    <div class="row">
+        <div class="col large-12 last-modify">
+            <i class="fa fa-calendar div_inline mr_5 ml_15" aria-hidden="true"></i>
+            <span class="hidden-xs">Cập nhật lần cuối:</span> <?php echo get_the_modified_date('H:s d/m/Y'); ?>
+        </div>
+    </div>
+<?php endif; ?>
+</main>
+
+<footer id="footer" class="footer-wrapper">
+
+    <?php do_action('flatsome_footer'); ?>
+
+</footer>
+
+</div>
+
+<?php wp_footer(); ?>
+
+</body>
+
+</html>
--- src/wp-content/themes/flatsome/template-parts/posts/partials/entry-title.php
+++ src/wp-content/themes/flatsome/template-parts/posts/partials/entry-title.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * Post-entry title.
  *
@@ -6,26 +7,21 @@
  * @flatsome-version 3.16.0
  */
 
-if ( is_single() ) :
-	if ( get_theme_mod( 'blog_single_header_category', 1 ) ) :
+if (is_single()) :
+	if (get_theme_mod('blog_single_header_category', 1)) :
 		echo '<h6 class="entry-category is-xsmall">';
-		echo get_the_category_list( __( ', ', 'flatsome' ) );
+		echo get_the_category_list(__(', ', 'flatsome'));
 		echo '</h6>';
 	endif;
 else :
 	echo '<h6 class="entry-category is-xsmall">';
-	echo get_the_category_list( __( ', ', 'flatsome' ) );
+	echo get_the_category_list(__(', ', 'flatsome'));
 	echo '</h6>';
 endif;
 
-if ( is_single() ) :
-	if ( get_theme_mod( 'blog_single_header_title', 1 ) ) :
+if (is_single()) :
+	if (get_theme_mod('blog_single_header_title', 1)) :
 		echo '<h1 class="entry-title">' . get_the_title() . '</h1>';
-		echo '<div class="img-title-goo">
-		<a href="https://news.google.com/publications/CAAqBwgKMIe8oAswlMa4Aw?hl=en-US&gl=US&ceid=US:en">
-		<img src="/wp-content/uploads/2023/06/google-news-HPSG.png" width="320"/>
-		</a>
-		</div>';
 	endif;
 else :
 	echo '<h2 class="entry-title"><a href="' . get_the_permalink() . '" rel="bookmark" class="plain">' . get_the_title() . '</a></h2>';
@@ -34,42 +30,53 @@ endif;
 
 
 ?>
-<?php if(get_field('tacgia') ): ?>
-	<div class="row author-hp">
-		<div class="col ">
+
+<div class="row author-hp">
+	<?php if (get_field('tacgia')) : ?>
+		<div class="large-6 ">
 			<div class="small-12 large-4 item  ">
 				<?php
-				if(get_field('tacgia')):
+				if (get_field('tacgia')) :
 					echo "<div class='div_flex bg_xam radius_15'>";
 					$id_tacgia = (int)get_field('tacgia');
-					if(has_post_thumbnail($id_tacgia)):
-						$thumb_src = get_the_post_thumbnail_url($id_tacgia,array(100,100));
+					if (has_post_thumbnail($id_tacgia)) :
+						$thumb_src = get_the_post_thumbnail_url($id_tacgia, array(100, 100));
 					endif;
-					echo "<a class='item' href='".get_the_permalink($id_tacgia)."' title='".get_the_title($id_tacgia)."'>";
-					echo '<img width="70" class="img-responsive img-circle  mr_10" src="'.$thumb_src.'" alt="'.get_the_title($id_tacgia).'">';
+					echo "<a class='item' href='" . get_the_permalink($id_tacgia) . "' title='" . get_the_title($id_tacgia) . "'>";
+					echo '<img width="70" class="img-responsive img-circle  mr_10" src="' . $thumb_src . '" alt="' . get_the_title($id_tacgia) . '">';
 					echo "</a>";
 					echo "<div class='item cl_33 pl_15'>";
 					echo "<span class='mb_5'>Đăng bởi</span>";
-					echo "<a class='' href='".get_the_permalink($id_tacgia)."' title='".get_the_title($id_tacgia)."'>";
-					echo "<b class=' name_bs font-weight-bold  small'>".get_the_title($id_tacgia)."</b><br><span class=' name_bs font-weight-bold  small'>".get_the_date('H:s d/m/Y')."</span>";
+					echo "<a class='' href='" . get_the_permalink($id_tacgia) . "' title='" . get_the_title($id_tacgia) . "'>";
+					echo "<b class=' name_bs font-weight-bold  small'>" . get_the_title($id_tacgia) . "</b><br><span class=' name_bs font-weight-bold  small'>" . get_the_date('H:s d/m/Y') . "</span>";
 					echo "</a>";
 					echo "</div>";
 					echo "</div>";
 				endif;
+
 				?>
 			</div>
 		</div>
+	<?php endif; ?>
+	<div class="large-6 ">
+		<div class="img-title-goo">
+			<a href="https://news.google.com/publications/CAAqBwgKMIe8oAswlMa4Aw?hl=en-US&gl=US&ceid=US:en">
+				<img src="/wp-content/uploads/2023/06/google-news-HPSG.png" width="320" />
+			</a>
+			<?php echo kk_star_ratings(); ?>
+		</div>
 	</div>
+</div>
 
-<?php endif; ?>
+
 
 <?php
-$single_post = is_singular( 'post' );
-if ( $single_post && get_theme_mod( 'blog_single_header_meta', 1 ) ) : ?>
+$single_post = is_singular('post');
+if ($single_post && get_theme_mod('blog_single_header_meta', 1)) : ?>
 	<div class="entry-meta uppercase is-xsmall">
 		<?php flatsome_posted_on(); ?>
 	</div>
-<?php elseif ( ! $single_post && 'post' == get_post_type() ) : ?>
+<?php elseif (!$single_post && 'post' == get_post_type()) : ?>
 	<div class="entry-meta uppercase is-xsmall">
 		<?php flatsome_posted_on(); ?>
 	</div>
