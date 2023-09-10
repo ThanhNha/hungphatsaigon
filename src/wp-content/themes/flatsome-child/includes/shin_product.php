373d1a33768ee6c4d03111043bbb13e37c776a6b
shin <nha.le@floatingcube.com>
2023-09-10 10:59:14 +0700

    handle case change page not found

--- src/wp-content/themes/flatsome-child/includes/shin_product.php
+++ src/wp-content/themes/flatsome-child/includes/shin_product.php
@@ -4,13 +4,12 @@
 function  filterPriceProduct()
 {
 
-  global $post;
-  $terms = get_the_terms($post->ID, 'product_cat');
-  $category_slug = $terms[0]->slug;
+  $category_slug =get_term(get_queried_object()->term_id, 'product_cat')->slug ;
+  $category_id =get_term(get_queried_object()->term_id) ;
   $args = array(
     'post_status' => 'publish',
     'limit' => -1,
-    'category' => '',
+    'category' => $category_slug,
     'paginate' => false,
   );
 
@@ -24,6 +23,7 @@ function  filterPriceProduct()
   sort($all_prices);
   $minPrice = $all_prices[array_key_first($all_prices)];
   $maxPrice = $all_prices[array_key_last($all_prices)];
+
 ?>
 <?php if($category_slug):?>
   <aside id="custom_html-7" class="widget_text widget widget_custom_html"><span class="widget-title shop-sidebar">MỨC GIÁ</span>
@@ -32,55 +32,55 @@ function  filterPriceProduct()
       <div class="loc_gia">
         <?php if ($minPrice < 5000000) : ?>
           <div class="radio">
-            <a href="?min_price=0&max_price=5000000"><input type="radio" name="optradio">Dưới 5
+            <a href="<?php  echo get_category_link($category_id);?>?min_price=0&max_price=5000000"><input type="radio" name="optradio">Dưới 5
               triệu</a>
           </div>
         <?php endif; ?>
         <?php if ($minPrice < 10000000) : ?>
           <div class="radio">
-            <a href="?min_price=0&max_price=10000000"><input type="radio" name="optradio">Dưới
+            <a href="<?php  echo get_category_link($category_id);?>?min_price=0&max_price=10000000"><input type="radio" name="optradio">Dưới
               10 triệu</a>
           </div>
         <?php endif; ?>
         <?php if ($minPrice < 15000000) : ?>
           <div class="radio">
-            <a href="?min_price=0&max_price=15000000"><input type="radio" name="optradio">Dưới
+            <a href="<?php  echo get_category_link($category_id);?>?min_price=0&max_price=15000000"><input type="radio" name="optradio">Dưới
               15 triệu</a>
           </div>
         <?php endif; ?>
         <?php if ($minPrice < 20000000) : ?>
           <div class="radio">
-            <a href="?min_price=0&max_price=20000000"><input type="radio" name="optradio">Dưới
+            <a href="<?php  echo get_category_link($category_id);?>?min_price=0&max_price=20000000"><input type="radio" name="optradio">Dưới
               20 triệu</a>
           </div>
         <?php endif; ?>
         <?php if ($minPrice < 30000000) : ?>
           <div class="radio">
-            <a href="?min_price=0&max_price=30000000"><input type="radio" name="optradio">Dưới
+            <a href="<?php  echo get_category_link($category_id);?>?min_price=0&max_price=30000000"><input type="radio" name="optradio">Dưới
               30 triệu</a>
           </div>
         <?php endif; ?>
         <?php if ($minPrice < 10000000) : ?>
           <div class="radio">
-            <a href="?min_price=5000000&max_price=10000000"><input type="radio" name="optradio">Từ
+            <a href="<?php  echo get_category_link($category_id);?>?min_price=5000000&max_price=10000000"><input type="radio" name="optradio">Từ
               5 triệu đến 10 triệu</a>
           </div>
         <?php endif; ?>
         <?php if ($minPrice < 20000000) : ?>
           <div class="radio">
-            <a href="?min_price=10000000&max_price=20000000"><input type="radio" name="optradio">Từ 10 triệu
+            <a href="<?php  echo get_category_link($category_id);?>?min_price=10000000&max_price=20000000"><input type="radio" name="optradio">Từ 10 triệu
               đến 20 triệu</a>
           </div>
         <?php endif; ?>
         <?php if ( $minPrice < 30000000) : ?>
           <div class="radio">
-            <a href="?min_price=20000000&max_price=30000000"><input type="radio" name="optradio">Từ 20 triệu
+            <a href="<?php  echo get_category_link($category_id);?>?min_price=20000000&max_price=30000000"><input type="radio" name="optradio">Từ 20 triệu
               đến 30 triệu</a>
           </div>
         <?php endif; ?>
         <?php if ($maxPrice > 30000000) : ?>
           <div class="radio">
-            <a href="?min_price=30000000"><input type="radio" name="optradio">Trên 30 triệu</a>
+            <a href="<?php  echo get_category_link($category_id);?>?min_price=30000000"><input type="radio" name="optradio">Trên 30 triệu</a>
           </div>
         <?php endif; ?>
       </div>
