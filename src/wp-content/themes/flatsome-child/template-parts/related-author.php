1bfc015c31eb3e1dfb0db3c303790af476b54a3c
shin <nha.le@floatingcube.com>
2023-09-17 14:37:43 +0700

    add code pagination

--- src/wp-content/themes/flatsome-child/template-parts/related-author.php
+++ src/wp-content/themes/flatsome-child/template-parts/related-author.php
@@ -7,16 +7,20 @@ if (!isset($post_type) || $post_type != 'tac-gia') {
 
 $id = get_queried_object()->ID;
 
-$posts = get_posts(array(
-  'numberposts'   => 10,
+$args = array(
   'post_type'     => 'post',
   'meta_key'      => 'tacgia',
   'meta_value'    => $id,
   'orderby'          => 'date',
-  'order'            => 'DESC',
+  'order'            => 'ASC',
   'include'          => array(),
   'exclude'          => array(),
-));
+  'posts_per_page' => '10',
+  'paged' => (get_query_var('paged') ? get_query_var('paged') : 1),
+);
+$posts = new WP_Query($args);
+
+$max_num_pages = $posts->max_num_pages;
 
 ?>
 
@@ -27,46 +31,105 @@ $posts = get_posts(array(
 
     </div>
 
-    <div class="row row-large large-columns-4 medium-columns-2 small-columns-1 slider row-slider slider-nav-simple slider-nav-outside slider-nav-push has-block is-draggable flickity-enabled tooltipstered" data-flickity-options='{
-          "imagesLoaded": true,
-          "groupCells":"100%",
-          "dragThreshold": 5,
-          "cellAlign": "left",
-          "wrapAround": true,
-          "prevNextButtons": true,
-          "percentPosition": true,
-          "pageDots": false,
-          "rightToLeft": false,
-          "autoPlay": false}' tabindex="0">
+    <div class="row">
+      <?php
+      if ($posts->have_posts()) : ?>
+        <?php while ($posts->have_posts()) : $posts->the_post(); ?>
 
-      <?php foreach ($posts as $post) : ?>
-
-        <div class="col post-item">
-          <div class="col-inner">
-            <div class="box box-bounce box-text-bottom box-blog-post has-hover">
-              <div class="box-image">
-                <div class="image-cover" style="padding-top:75%;">
-                  <a href="<?php echo get_the_permalink(); ?>" class="plain" aria-label="<?php echo get_the_title(); ?>">
-                    <img width="300" height="158" src="<?php echo get_the_post_thumbnail_url(); ?>" class="attachment-medium size-medium wp-post-image" alt="<?php echo get_the_title(); ?>" decoding="async" loading="lazy" sizes="(max-width: 300px) 100vw, 300px"> </a>
+          <div class="col post-item">
+            <div class="col-inner">
+              <div class="row box box-bounce box-text-bottom box-blog-post has-hover">
+                <div class="box-image col medium-5 large-3">
+                  <div class="image-cover" style="padding-top:75%;">
+                    <a href="<?php echo get_the_permalink(); ?>" class="plain" aria-label="<?php echo get_the_title(); ?>">
+                      <img width="300" height="158" src="<?php echo get_the_post_thumbnail_url(); ?>" class="attachment-medium size-medium wp-post-image" alt="<?php echo get_the_title(); ?>" decoding="async" loading="lazy" sizes="(max-width: 300px) 100vw, 300px"> </a>
+                  </div>
+                </div>
+                <div class="box-text col medium-7 large-9">
+                  <div class="box-text-inner blog-post-inner">
+                    <h4 class="post-title is-large ">
+                      <a href="<?php echo get_the_permalink(); ?>" class="plain"><?php echo get_the_title(); ?></a>
+                    </h4>
+                    <p class="from_the_blog_excerpt "><?php echo get_the_excerpt(); ?></p>
+                  </div>
                 </div>
               </div>
-              <div class="box-text text-center">
-                <div class="box-text-inner blog-post-inner">
-                  <h4 class="post-title is-large ">
-                    <a href="<?php echo get_the_permalink(); ?>" class="plain"><?php echo get_the_title(); ?></a>
-                  </h4>
-                  <div class="img-title-goo"><a href="https://news.google.com/publications/CAAqBwgKMIe8oAswlMa4Aw?hl=en-US&amp;gl=US&amp;ceid=US:en"><img src="/wp-content/uploads/2023/06/google-news-HPSG.png" width="320"></a></div>
-                  <div class="is-divider"></div>
-                  <p class="from_the_blog_excerpt "><?php echo get_the_excerpt(); ?></p>
-                </div>
-              </div>
+
             </div>
-
           </div>
-        </div>
-      <?php endforeach; ?>
+        <?php endwhile; ?>
+      <?php endif; ?>
 
     </div>
-
+    <div class="row">
+      <?php pagination_post_author($max_num_pages); ?>
+    </div>
   </div>
 </section>
+
+
+<?php
+function pagination_post_author($max_num_pages = 0)
+
+{
+
+  if ($max_num_pages <= 1)
+    return;
+  $paged = get_query_var('paged') ? absint(get_query_var('paged')) : 1;
+
+  if ($paged >= 1)
+    $links[] = $paged;
+
+  if ($paged >= 3) {
+    $links[] = $paged - 1;
+    $links[] = $paged - 2;
+  }
+
+  if (($paged + 2) <= $max_num_pages) {
+    $links[] = $paged + 2;
+    $links[] = $paged + 1;
+  }
+?>
+  <nav class="woocommerce-pagination">
+    <ul class="page-numbers nav-pagination links text-center">
+      <?php if ($paged > 1) : ?>
+        <li class="item_k"><a href="<?php echo esc_url(get_pagenum_link($paged - 1)); ?>"><i class="icon-angle-left"><span class="screen-reader-text hidden">prev</span></i></a></li>
+      <?php endif; ?>
+      <?php if (get_previous_posts_link()) : ?>
+        <li><?php echo  get_previous_posts_link(); ?></li>
+      <?php endif; ?>
+      <?php if (!in_array(1, $links)) : ?>
+        <?php $class = 1 == $paged ? 'page-numbers current' : ''; ?>
+
+        <li><a class="<?php echo $class; ?>" href=" <?php echo esc_url(get_pagenum_link(1)) ?>">1
+          </a></li>
+      <?php endif; ?>
+
+      <?php
+
+      sort($links);
+      foreach ((array) $links as $link) {
+        $class = $paged == $link ? 'page-numbers current' : '';
+        printf('<li><a class="%s" href="%s">%s</a></li>' . "\n", $class, esc_url(get_pagenum_link($link)), $link);
+      }
+      ?>
+      <?php if (get_next_posts_link()) : ?>
+
+        <li class="item_k">
+          <?php echo get_next_posts_link(); ?>
+        </li>
+      <?php endif; ?>
+      <?php
+      if (!in_array($max_num_pages, $links)) {
+        if (!in_array($max_num_pages - 1, $links))
+          $class = $paged == $max_num_pages ? ' class="page-numbers current"' : '';
+        printf('<li class="%s"><a href="%s">%s</a></li>' . "\n", $class, esc_url(get_pagenum_link($max_num_pages)), $max_num_pages);
+      }
+      ?>
+      <?php if ($paged < $max_num_pages) : ?>
+        <li class="item_k"><a href="<?php echo esc_url(get_pagenum_link($paged + 1)); ?>"><i class="icon-angle-right"><span class="screen-reader-text hidden">prev</span></i></a></li>
+      <?php endif; ?>
+    </ul>
+  </nav>
+<?php
+}
